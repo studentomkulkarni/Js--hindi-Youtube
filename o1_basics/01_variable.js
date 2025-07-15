715d3e98a296357b432a11b38e9c1630309fceb9
Om Kulkarni <144256029+studentomkulkarni@users.noreply.github.com>
2025-07-15 08:51:45 +0000

    let const and var in javascript

---
+++ o1_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=1234556
+let accountEmail="ommk2005@gmail.com";
+var accountpassword="1234567";
+accountCity="Pune";
+console.log(accountId)
+console.table([accountId,accountEmail,accountpassword,accountCity])
+let accountstate
+accountEmail="omdhdh@gmail.com"
+accountpassword="1792020";
+accountCity="Nashik"
+console.table([accountId,accountEmail,accountpassword,accountCity,accountstate])
+console.log("om")
+
+
