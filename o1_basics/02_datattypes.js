0e8854310fa411cee4ceccf818f2686336407615
Om Kulkarni <144256029+studentomkulkarni@users.noreply.github.com>
2025-07-15 09:24:23 +0000

    Datatypes in javascript

---
+++ o1_basics/02_datattypes.js
@@ -0,0 +1,20 @@
+//number == 2 to power 53
+/*
+bigint
+string
+boolean= true/false
+null=> standalone value
+undefined
+symbol=> unique
+
+Object*/
+ let name="Om"
+ let age=20
+ console.log(typeof name)
+ console.log(typeof age)
+ console.log(typeof undefined)//Important for iterview question difference between type of undefined nd null because type of undefined is undefined and type of null is object in Js
+ 
+ console.log(typeof null)
+
+
+ 
