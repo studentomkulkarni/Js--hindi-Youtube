5b6d6115f4fc84387e699bc3d668bf4ca8315a19
Om Kulkarni <144256029+studentomkulkarni@users.noreply.github.com>
2025-07-16 11:54:52 +0000

    data type convesion

---
+++ o1_basics/conversion_basics.js
@@ -0,0 +1,35 @@
+let number ="33"
+console.log(typeof number);
+let answer =Number(number)
+console.log(number);
+console.log(typeof(answer));
+
+let isloggedin = "Sounak"
+console.log(isloggedin)
+console.log(typeof(isloggedin))
+let ans =Boolean(isloggedin)
+console.log(typeof(ans))
+console.log(ans)
+
+var issloggedin ="";
+//console.log(issloggedin)
+let abc = Boolean(issloggedin);
+console.log(typeof abc)
+console.log(abc)
+
+let cbd=null
+console.log(typeof cbd)
+num =Number(cbd)
+console.log(cbd)
+
+
+let str=2000
+let score=String(str)
+console.log(typeof(score))
+console.log(str)
+
+console.log(2+2)
+
+
+
+
